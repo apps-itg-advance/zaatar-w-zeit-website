ee78525c5cca3fd9c188da3339293be00e71475b
Abdallah Monzer <AbdallahMonzer@itb-me.com>
2020-03-05 10:31:51 +0200

    ui fixes

--- resources/views/menu/_menu_details.blade.php
+++ resources/views/menu/_menu_details.blade.php
@@ -50,7 +50,7 @@
                         <div class="col-lg-4 col-md-12 item-col">
                             <div class="custom-control custom-radio mb-1">
                                 <input type="checkbox"  value="{{$make_meal->ID.'-'.$make_meal->Price.'-'.$make_meal->Details}}"  onclick="CalculateMakeMealTotal({{$make_meal->ID}},{{$row->ID}})"  id="makeMealH{{$make_meal->ID}}" name="make_meal[{{$row->ID}}][Title]" class="custom-control-input">
-                                <label class="custom-control-label text-uppercase" for="makeMealH{{$make_meal->ID}}">
+                                <label class="custom-control-label text-uppercase futura-b" for="makeMealH{{$make_meal->ID}}">
                                     {{$make_meal->Title}}
                                     <span class="price">{{$make_meal->Price}}</span>
                                 </label>
