daed2cdd6342afcf112ab53b687aa107e0943c6f
Abdallah Monzer <AbdallahMonzer@itb-me.com>
2020-03-10 17:32:22 +0200

    fixes

--- resources/views/partials/left.blade.php
+++ resources/views/partials/left.blade.php
@@ -19,7 +19,7 @@
                 @endphp
                     <a href="{{route('home.menu', ['id'=>$nav->ID,'name'=>$url_name])}}">
                 <div class="media @if($nav->ID==$cat_id) active  @endif">
-                        <img src="{{$nav->URL}}" style="width: 60px !important; height: 60px !important; border-radius: 50%;" class="mr-3" alt="...">
+                        <img src="{{$nav->URL}}" style="!important; border-radius: 50%;" class="mr-3" alt="...">
                         <div class="align-self-center media-body">
                             <h5 class="mt-0">
                                 {{strtoupper($url_name)}}
