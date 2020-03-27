c434effb9a744f926bf974dd560485aca0d806fe
mhammad massarra <mohamad@dotcomlb.com>
2020-03-27 10:44:35 +0200

    Update _make_meal.blade.php

--- resources/views/menu/_make_meal.blade.php
+++ resources/views/menu/_make_meal.blade.php
@@ -36,7 +36,7 @@
                                 @foreach($meal_items as $meal_item)
                                     <div class="custom-control custom-radio custom-control-inline">
                                         <input type="checkbox" value="{{$meal_item->ID.'-'.$meal_item->PLU.'-'.$make_meal->ID.'-'.$meal_item->Name}}" id="makeMealQ{{$meal_item->ID}}" name="make_meal[{{$row->ID}}][Items][{{$meal_item->ID}}]" class="custom-control-input Sub{{$make_meal->ID}}" disabled>
-                                        <label class="custom-control-label" for="makeMealQ{{$meal_item->ID}}">{{$meal_item->Name}}</label>
+                                        <label class="custom-control-label" style="font-size: 15px !important;" for="makeMealQ{{$meal_item->ID}}">{{$meal_item->Name}}</label>
                                     </div>
                                 @endforeach
                                 @endif
