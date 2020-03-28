9aa220f00b18bc8dc184a9a8da5ab6c9f3fe5cbb
mhammad massarra <mohamad@dotcomlb.com>
2020-03-28 12:46:41 +0200

    Update _edit_profile.blade.php

--- resources/views/customers/_edit_profile.blade.php
+++ resources/views/customers/_edit_profile.blade.php
@@ -21,13 +21,13 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <label>Name</label>
-                            <input type="text" class="form-control" id="FirstName" name="first_name{{$Skey}}" value="{{@$details->FirstName}}" />
+                            <input type="text" class="form-control" maxlength="30" id="FirstName" name="first_name{{$Skey}}" value="{{@$details->FirstName}}" />
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label>Family Name</label>
-                            <input type="text" class="form-control" id="LastName" name="last_name{{$Skey}}" value="{{@$details->LastName}}" />
+                            <input type="text" class="form-control" maxlength="30" id="LastName" name="last_name{{$Skey}}" value="{{@$details->LastName}}" />
                         </div>
                     </div>
                     <div class="col-md-6">
