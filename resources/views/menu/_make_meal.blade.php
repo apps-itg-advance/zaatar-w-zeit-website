c55f27454685fd9f80cd075eeb028e516dca65af
mhammad massarra <mohamad@dotcomlb.com>
2020-05-12 16:42:49 +0300

    Update _make_meal.blade.php

--- resources/views/menu/_make_meal.blade.php
+++ resources/views/menu/_make_meal.blade.php
@@ -35,7 +35,7 @@
                                 @if(is_array($meal_items) and count($meal_items)>0)
                                 @foreach($meal_items as $meal_item)
                                     <div class="custom-control custom-radio custom-control-inline">
-                                        <input type="radio" value="{{$meal_item->ID.'-'.$meal_item->PLU.'-'.$make_meal->ID.'-'.$meal_item->Name}}" id="makeMealQ{{$meal_item->ID}}" name="make_meal[{{$row->ID}}][Items][{{$make_meal->ID}}]" class="custom-control-input Sub{{$make_meal->ID}}" disabled>
+                                        <input type="radio" value="{{$meal_item->ID.'-'.$meal_item->PLU.'-'.$make_meal->ID.'-'.$meal_item->Name}}" id="makeMealQ{{$meal_item->ID.'-'.$make_meal->ID}}" name="make_meal[{{$row->ID}}][Items][{{$make_meal->ID}}]" class="custom-control-input Sub{{$make_meal->ID}}" disabled>
                                         <label class="custom-control-label list-i" for="makeMealQ{{$meal_item->ID}}">{{$meal_item->Name}}</label>
                                     </div>
                                 @endforeach
