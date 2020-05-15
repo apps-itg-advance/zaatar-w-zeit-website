4930ae90f89ff4e36fd3782c32b3ac019b2f94bd
mhammad massarra <mohamad@dotcomlb.com>
2020-05-15 15:13:19 +0300

    u

--- resources/views/customers/_favourite_menu.blade.php
+++ resources/views/customers/_favourite_menu.blade.php
@@ -1,7 +1,7 @@
 @php
 $active='btn-8DBF43 ';
 @endphp
-<div class="title-div mb-4 pb-2 text-left col-xl-6 col-lg-12 col-favourite fav-padding">
+<div class="title-div mb-4 pb-2 text-left col-xl-6 col-lg-12 fav-padding">
     <a href="{{route('customer.favourite.items')}}" class="btn btn-fav {{(!isset($sub_active) or $sub_active=='fav')? $active : 'btn-orders'}} mr-2">Favourites</a>
     <a href="{{route('customer.favourite.orders')}}" class="btn {{isset($sub_active) && $sub_active=='orders'? $active : 'btn-orders'}}">Orders</a>
 </div>
