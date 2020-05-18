6b6806d3e10f64b1179dd02e8a31bab3f385ecb5
mhammad massarra <mohamad@dotcomlb.com>
2020-05-18 15:01:44 +0300

    up

--- resources/views/partials/_cart.blade.php
+++ resources/views/partials/_cart.blade.php
@@ -67,7 +67,7 @@
 
     </div>
     <div class="action-buttons text-center mt-5 mb-3">
-        <button class="btn btn-B3B3B3 text-uppercase btn-B3B3B3-non-hover" onclick="_destroyCart()" id="DestroyBtn">Clear All</button>
+        <button @if($cart!=null) class="btn btn-B3B3B3 text-uppercase" @else class="btn btn-B3B3B3 text-uppercase btn-B3B3B3-non-hover" @endif onclick="_destroyCart()" id="DestroyBtn">Clear All</button>
         <a id="CheckOutBtn" @if($cart!=null) href="{{route('checkout.address')}}" class="btn btn-8DBF43 text-uppercase " @else href="javascript:void(0)" class="btn btn-B3B3B3 text-uppercase btn-B3B3B3-non-hover" @endif >Checkout</a>
     </div>
 
