d2a2073c4ccc446e3fcd77330540dd2167724d4d
mhammad massarra <mohamad@dotcomlb.com>
2020-06-15 15:26:09 +0300

    Update template.blade.php

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,7 +6,7 @@
 {{--<html lang="{{App::getLocale()}}" dir="{{(App::isLocale('ar') ? 'rtl' : 'ltr')}}">--}}
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
